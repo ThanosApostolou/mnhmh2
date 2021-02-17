e2d218530182e1168573f1a9b8880ecdd89a8b4c
ThanosApostolou <thanosapostolou@outlook.com>
2021-02-17 12:50:53 +0200

    update structure

---
+++ mnhmh2-server/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "strict":                   "error",
+        "semi":                     "warn",
+        "no-template-curly-in-string":"error",
+        "block-scoped-var":         "error",
+        "class-methods-use-this":   "error",
+        "consistent-return":        "error",
+        "no-param-reassign":        "error",
+        "no-invalid-this":          "error",
+        "init-declarations":        "warn",
+        "no-label-var":             "error",
+        "no-undef-init":            "error",
+        "no-undefined":             "error",
+        "no-use-before-define":     "error",
+        "class-methods-use-this":   "warn",
+        "indent":                   [1, 4],
+        "quotes":                   [1, "double"],
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-inferrable-types": 1,
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-extra-semi": 1,
+        "@typescript-eslint/indent": [1, 4]
+    }
+};
---
+++ mnhmh2-server/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.ts",
+    target: 'node',
+    output: {
+        path: path.join(__dirname, "/build"),
+        filename: "mnhmh2-server.js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: {
+                    loader: "ts-loader"
+                },
+                exclude: [/node_modules/, /build/]
+            },
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/, /build/]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: 'assets/[name][ext][query]'
+                },
+                exclude: [/node_modules/, /build/]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ESLintPlugin({
+            context: "src/",
+            extensions: ["js", "ts"],
+            fix: true
+        })
+    ]
+};
