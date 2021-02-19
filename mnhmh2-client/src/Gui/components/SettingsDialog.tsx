5d24b5fa62f3de8c59561bc27af3f74cb669336a
ThanosApostolou <thanosapostolou@outlook.com>
2021-02-19 12:31:02 +0200

    udpate settings dialog

--- mnhmh2-client/src/Gui/components/SettingsDialog.tsx
+++ mnhmh2-client/src/Gui/components/SettingsDialog.tsx
@@ -1,10 +1,14 @@
 import React, { ReactNode } from "react";
-import { Button, Dialog, DialogContent, DialogActions } from "@material-ui/core";
+import { Button, Dialog, DialogContent, DialogActions, TextField, Switch, FormControlLabel, DialogTitle } from "@material-ui/core";
 
 
 export class SettingsDialog extends React.Component<any, any> {
     constructor(props) {
         super(props);
+        this.state = {
+            settingsChanged: false,
+            serverSettingIsDefault: false,
+        };
     }
     /**
      * Cancels settings changes and calls parent's handleCloseSettingsDialog()
@@ -20,17 +24,25 @@ export class SettingsDialog extends React.Component<any, any> {
         console.log("handleSave");
         this.props.handleCloseSettingsDialog();
     }
+
     render(): ReactNode {
         return (
             <Dialog open={this.props.openSettingsDialog}  fullWidth={true} maxWidth="xl" disableBackdropClick disableEscapeKeyDown>
+                <DialogTitle style={{textAlign: "center"}}>
+                    Settings
+                </DialogTitle>
                 <DialogContent>
-                    adf
+                    <TextField label="Server Address:" defaultValue="http://localhost:3000" disabled={this.state.serverSettingIsDefault } />
+                    <FormControlLabel
+                        control={<Switch name="default" checked={this.state.serverSettingIsDefault} onChange={() => this.setState({serverSettingIsDefault: !this.state.serverSettingIsDefault})} />}
+                        label="default"
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button autoFocus onClick={this.handleCancel.bind(this)}>
                         Cancel
                     </Button>
-                    <Button onClick={this.handleSave.bind(this)}>
+                    <Button disabled={!this.state.changed} onClick={this.handleSave.bind(this)}>
                         Save
                     </Button>
                 </DialogActions>
