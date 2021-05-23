4f8eecdf0bdf021d57dc596defbb98ed34b0f116
ThanosApostolou <thanosapostolou@outlook.com>
2021-05-23 11:46:52 +0300

    add export to xlsx

--- mnhmh2-client/src/Gui/components/MyDataGrid/MyGridExportComp.tsx
+++ mnhmh2-client/src/Gui/components/MyDataGrid/MyGridExportComp.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode } from "react";
+import XLSX from "xlsx";
 
 import { Button, Popover, MenuItem } from "@material-ui/core";
 import { GetApp } from "@material-ui/icons";
-import { GridColDef, GridRowsProp } from "@material-ui/data-grid";
+import { GridColDef, GridRowData, GridRowsProp } from "@material-ui/data-grid";
 
 export class MyGridExportComp extends React.Component<MyGridExportCompProps, MyGridExportCompState> {
     state: Readonly<MyGridExportCompState>;
@@ -16,39 +17,71 @@ export class MyGridExportComp extends React.Component<MyGridExportCompProps, MyG
 
     downloadJSON(): void {
         this.setState({anchorMenuEl: null});
-        const columns = [];
+        const header = [];
         for (const column of this.props.columns) {
-            columns.push(column.headerName);
+            if (!column.hide) {
+                header.push(column.headerName);
+            }
+        }
+        const rows = [];
+        for (const row of this.props.rows) {
+            const datarow = [];
+            for (const [key, value] of Object.entries(row)) {
+                for (const column of this.props.columns) {
+                    if (column.field === key) {
+                        if (!column.hide) {
+                            datarow.push(value);
+                        }
+                        break;
+                    }
+                }
+            }
+            rows.push(datarow);
         }
         const data = JSON.stringify({
-            columns: columns,
-            rows: this.props.rows
+            header: header,
+            rows: rows
         }, null, 4);
         const blob = new Blob([data], { type: "application/json" });
         const link = document.createElement("a");
         link.href = window.URL.createObjectURL(blob);
         link.download = "data.json";
-        document.body.appendChild(link);
         link.click();
         setTimeout(() => {
             window.URL.revokeObjectURL(link.href);
-            document.body.removeChild(link);
         }, 100);
     }
 
     downloadXLSX(): void {
         this.setState({anchorMenuEl: null});
-        const list = ["a", "b"];
-        const data = new Blob([list.join("\n")], { type: "text/plain" });
-        const link = document.createElement("a");
-        link.href = window.URL.createObjectURL(data);
-        link.download = "data.json";
-        document.body.appendChild(link);
-        link.click();
-        setTimeout(() => {
-            window.URL.revokeObjectURL(link.href);
-            document.body.removeChild(link);
-        }, 100);
+        const header = []; // header row
+        const wscols = []; // columns style
+        for (const column of this.props.columns) {
+            if (!column.hide) {
+                header.push(column.headerName);
+                wscols.push({wpx: column.width});
+            }
+        }
+        const datarows = [header]; // all data
+        for (const row of this.props.rows) {
+            const datarow = []; // single row
+            for (const [key, value] of Object.entries(row)) {
+                for (const column of this.props.columns) {
+                    if (column.field === key) {
+                        if (!column.hide) {
+                            datarow.push(value);
+                        }
+                        break;
+                    }
+                }
+            }
+            datarows.push(datarow);
+        }
+        const ws = XLSX.utils.aoa_to_sheet(datarows);
+        ws["!cols"] = wscols;
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Data");
+        XLSX.writeFile(wb, "data.xlsx");
     }
 
     render(): ReactNode {
